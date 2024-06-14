d82baf914c299289e0bb8de9e5dfb3fbd04c5fd8
Jerin <jerinalour@gmail.com>
2024-06-14 12:29:23 +0530

    finshing_pageone

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 // src/components/navbar/Navbar.js
 import React, { useState } from 'react';
 import './Navbar.css';
-import Map_Icon from './Map_Icon.png';
+import MapIcon from './Map_Icon.png';
 import map from './map.png';
+import IIcon from './IIcon.png'; // Assuming you have an I_Icon.png file for the new button
 
 const Navbar = () => {
   const [showMap, setShowMap] = useState(false);
@@ -11,23 +12,43 @@ const Navbar = () => {
     setShowMap(!showMap);
   };
 
+  const handleBackButtonClick = () => {
+    setShowMap(false);
+  };
+
+  const handleIIconClick = () => {
+    // Handle the click event for the I Icon button
+  };
+
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-left">
+          <img 
+            src={IIcon} 
+            alt="I Icon" 
+            className="i-icon" 
+            onClick={handleIIconClick} 
+          />
           <h1>Treasure Hunt</h1>
         </div>
         <div className="navbar-right">
-          <img 
-            src= {Map_Icon} 
-            alt="Map Icon" 
-            className="map-icon" 
-            onClick={handleMapIconClick} 
-          />
+          {showMap ? (
+            <button className="back-button" onClick={handleBackButtonClick}>
+              Back
+            </button>
+          ) : (
+            <img 
+              src={MapIcon} 
+              alt="Map Icon" 
+              className="map-icon" 
+              onClick={handleMapIconClick} 
+            />
+          )}
         </div>
       </nav>
       {showMap && (
-        <div className="overlay">
+        <div className="overlay" onClick={handleMapIconClick}>
           <div className="map-container">
             <img src={map} alt="Map" className="map-image" />
           </div>
