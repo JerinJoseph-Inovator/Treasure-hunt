fbd0b2f8adbb410151fbe56d2bed62e633b29da6
Jerin <jerinalour@gmail.com>
2024-06-14 18:03:22 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Start from './components/start/start';
 import Navbar from './components/Navbar/Navbar';
 import QuestOne from './components/quest_one/quest_one';
-import QuestTwo from './components/quest_two/quest_two'; // Import QuestTwo
-import QuestThree from './components/quest_three/quest_three'; // Import QuestTwo
+import QuestTwo from './components/quest_two/quest_two';
+import QuestThree from './components/quest_three/quest_three';
 
 function App() {
   return (
@@ -13,12 +14,13 @@ function App() {
       <div className="App">
         <Navbar />
         <header className="App-header">
-        <Routes>
-          <Route path="/Start" element={<Start />} />
-          <Route path="/quest_one" element={<QuestOne />} />
-          <Route path="/quest_two" element={<QuestTwo />} /> {/* Add route for QuestTwo */}
-          <Route path="/quest_three" element={<QuestThree />} /> {/* Add route for QuestTwo */}
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Start />} />
+            <Route path="/start" element={<Start />} />
+            <Route path="/quest_one" element={<QuestOne />} />
+            <Route path="/quest_two" element={<QuestTwo />} />
+            <Route path="/quest_three" element={<QuestThree />} />
+          </Routes>
         </header>
       </div>
     </Router>
